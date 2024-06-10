9b543b142a193c2a842046b3125552122a490dc3
Radka Jersakova <r.jersakova@gmail.com>
2024-06-10 14:01:31 +0100

    mv extract function

--- who_is_that/extract.py
+++ who_is_that/extract.py
@@ -1,4 +1,18 @@
+from ebooklib import epub
+import ebooklib
+from bs4 import BeautifulSoup
+
+
 class Extractor:
     def process(self, epub_path: str) -> str:
         print(f"Extracting text from EPUB {epub_path}")
-        return ""
+
+        book = epub.read_epub(epub_path)
+        text_content = []
+
+        for item in book.get_items():
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                soup = BeautifulSoup(item.get_content(), 'lxml')
+                text_content.append(soup.get_text())
+
+        return '\n'.join(text_content)
