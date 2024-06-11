474e8f597c4058ce15e198fe7ddbf193c9dc9f9b
James Robinson <james.em.robinson@gmail.com>
2024-06-11 14:55:35 +0100

    :wrench: Symmetrise who_is_that and who_is_that_really

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import yaml
 import pathlib
 
 app = Flask(__name__)
-client = Client(host="http://localhost:11434")
+client = Client(host="http://ollama:11434")
 
 # Load the prompt template once at the start of the application
 data_dir = pathlib.Path(__file__).parent.resolve()
@@ -12,82 +12,119 @@ with open(data_dir / "prompts" / "input_prompt.txt", "r") as prompt_file:
     prompt_template = prompt_file.read()
 
 # Load the yaml file as a global variable
-with open(data_dir / "spoilerdb" / "database.yml", 'r') as database_file:
+with open(data_dir / "spoilerdb" / "database.yml", "r") as database_file:
     db = yaml.safe_load(database_file)
+
 
 @app.route("/")
 def index():
     return "Backend is running"
 
-def who_is_that(context, prompt_template, character):
+
+def who_is_that(context: str, prompt_template: str, character: str) -> str:
     prompt = prompt_template.replace("{character}", character)
     concat = f"CONTEXT: {context} \n INSTRUCTIONS: {prompt}"
+    try:
+        app.logger.info(f"Waiting for an Ollama response.")
+        response = client.chat(
+            model="llama3:8b",
+            messages=[
+                {
+                    "role": "user",
+                    "content": concat,
+                },
+            ],
+        )
+        content = response["message"]["content"]
+    except Exception as exc:
+        print(f"Failed to retrieve summary from Ollama {str(exc)}")
+        response = {"message": {"content": "some text here"}}
+        content = "Sorry, I could not answer your query."
+    return content
+
+
+def who_is_that_really(
+    model: str,
+    text: str,
+    book: str,
+    bookmark: str,
+    character: str | None = None,
+    place: str | None = None,
+) -> str:
+    """
+    Get a summary of the character's actions up to the bookmark in the text.
+    This function uses the LLM to generate a summary from the supplied text.
+    """
+
+    def generate_summary():
+        query = "I have written the following story: '" + text + "'."
+        query += " Read up to the end of " + bookmark + "."
+        if character:
+            query += (
+                " Describe what " + character + " has done so far in 15 word or less."
+            )
+            query += " Focus on key events and actions taken by this character."
+        elif place:
+            query += (
+                " Create a description of the location '"
+                + place
+                + "' in 15 word or less."
+            )
+        else:
+            query += " Describe the story so far in 15 word or less."
+        query += " Do not reveal spoilers for later sections of the story."
+        try:
+            app.logger.info(f"Waiting for an Ollama response.")
+            response = client.chat(
+                model=model,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": query,
+                    },
+                ],
+            )
+            content = response["message"]["content"]
+        except Exception as exc:
+            print(f"Failed to retrieve summary from Ollama {str(exc)}")
+            response = {"message": {"content": "some text here"}}
+            content = "Sorry, I could not answer your query."
+        return content
+
+    summary = generate_summary()
+    if character:
+        has_spoiler = spoiler_check(book, character, summary, model)
+        while has_spoiler:
+            summary = generate_summary()
+            has_spoiler = spoiler_check(book, character, summary, model)
+    return summary
+
+
+def spoiler_check(book: str, character: str, summary: str, model: str) -> bool:
+    if book not in db or character not in db[book]["characters"]:
+        return "No spoilers in the database for " + character + " in " + book + "."
+    query = "Read the following summary of " + character
+    query += " from '" + book + "' by " + db[book]["author"] + ": '" + summary
+    query += "'. Check to see whether the following spoiler is present: '"
+    query += db[book]["characters"][character]["spoilers"][0] + "'. "
+    query += "Give me a simple true or false answer."
     response = client.chat(
-        model="gemma",
+        model=model,
         messages=[
             {
                 "role": "user",
-                "content": concat,
+                "content": query,
             },
         ],
     )
-    return response["message"]["content"]
-
-
-def who_is_that_really(model, text, book, bookmark, character=None, place=None):
-  """
-    Get a summary of the character's actions up to the bookmark in the text.
-    This function uses the LLM to generate a summary from the supplied text.
-  """
-  def generate_summary():
-    query = "I have written the following story: '" + text + "'."
-    query += " Read up to the end of " + bookmark + "."
-    if character:
-      query += " Describe what " +  character + " has done so far in 15 word or less."
-      query += " Focus on key events and actions taken by this character."
-    elif place:
-      query += " Create a description of the location '" +  place + "' in 15 word or less."
+    answer = response["message"]["content"]
+    if "true" in answer or "True" in answer or "TRUE" in answer:
+        print("Spoiler detected! Regenerating summary...")
+        return True
+    elif "false" in answer or "False" in answer or "FALSE" in answer:
+        return False
     else:
-      query += " Describe the story so far in 15 word or less."
-    query += " Do not reveal spoilers for later sections of the story."
-    response = client.chat(model=model, messages=[
-      {
-        'role': 'user',
-        'content': query,
-      },
-    ])
-    return response['message']['content']
-  summary = generate_summary()
-  if character:
-    has_spoiler = spoiler_check(book, character, summary, model)
-    while has_spoiler:
-      summary = generate_summary()
-      has_spoiler = spoiler_check(book, character, summary, model)
-  return summary
-
-
-def spoiler_check(book, character, summary, model):
-  if book not in db or character not in db[book]['characters']:
-    return "No spoilers in the database for " + character + " in " + book + "."
-  query = "Read the following summary of " + character
-  query += " from '" +  book + "' by " +  db[book]['author'] + ": '" + summary
-  query += "'. Check to see whether the following spoiler is present: '"
-  query += db[book]['characters'][character]['spoilers'][0] + "'. "
-  query += "Give me a simple true or false answer."
-  response = client.chat(model=model, messages=[
-    {
-      'role': 'user',
-      'content': query,
-    },
-  ])
-  answer = response['message']['content']
-  if 'true' in answer or 'True' in answer or 'TRUE' in answer:
-    print("Spoiler detected! Regenerating summary...")
-    return True
-  elif 'false' in answer or 'False' in answer or 'FALSE' in answer:
-    return False
-  else:
-    raise Exception("Unexpected response from spoiler detection: " + answer)
+        raise Exception("Unexpected response from spoiler detection: " + answer)
 
 
 @app.route("/who_is_that", methods=["POST"])
