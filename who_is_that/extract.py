ce5a6051b51360dd97cdda6709d7e090d61a1563
Radka Jersakova <r.jersakova@gmail.com>
2024-06-11 11:42:56 +0100

    add Extractor.get_metadata, use it to return author and title info

--- who_is_that/extract.py
+++ who_is_that/extract.py
@@ -3,11 +3,22 @@ import ebooklib
 from bs4 import BeautifulSoup
 import tempfile
 import pathlib
+import typing
 
 class Extractor:
-    def __init__(self, text_content: list[tuple[str, str]]) -> None:
+    def __init__(self, text_content: list[tuple[str, str]], author: typing.Optional[str] = None, title: typing.Optional[str] = None) -> None:
         self.text_content = text_content
+        self.author = author
+        self.title = title
 
+    @staticmethod
+    def get_metadata(epub_path: pathlib.Path, data_type: str) -> str:
+        book = epub.read_epub(epub_path)
+        metadata = book.get_metadata('DC', data_type)
+        if metadata:
+            return metadata[0][0]
+        else:
+            return f"{data_type} not found"
 
     @staticmethod
     def process(epub_path: pathlib.Path) -> list[tuple[str, str]]:
@@ -32,7 +43,11 @@ class Extractor:
         tf = tempfile.NamedTemporaryFile()
         print(f"Writing to {tf.name}")
         tf.write(epub_contents)
-        obj = cls(text_content=cls.process(tf.name))
+        obj = cls(
+            text_content=cls.process(tf.name), 
+            author=cls.get_metadata(tf.name, 'creator'), 
+            title=cls.get_metadata(tf.name, 'title')
+        )
         # tf.delete()
         return obj
 
