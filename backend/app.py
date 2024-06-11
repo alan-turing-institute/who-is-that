4e36752331ffd87dceeb31273a5c5fae8110d97e
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-06-11 12:35:21 +0100

    who is that really?

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from ollama import Client
-import pathlib
+import yaml
 
 app = Flask(__name__)
 client = Client(host="http://localhost:11434")
@@ -9,6 +9,9 @@ client = Client(host="http://localhost:11434")
 with open("prompts/input_prompt.txt", "r") as file:
     prompt_template = file.read()
 
+# Load the yaml file as a global variable
+with open('spoilerdb/database.yml', 'r') as file:
+  db = yaml.safe_load(file)
 
 @app.route("/")
 def index():
@@ -27,6 +30,32 @@ def who_is_that(context, prompt_template, character):
         ],
     )
     return response["message"]["content"]
+
+
+def who_is_that_really(text, book, bookmark, character, model):
+  """
+    Get a summary of the character's actions up to the bookmark in the text.
+    This function uses the LLM to generate a summary from the supplied text.
+  """
+  def generate_summary():
+    query = "I have written the following story: '" + text + "'."
+    query += " Read up to the end of " + bookmark + "."
+    query += " Describle what " +  character + " has done so far in 15 word or less."
+    query += " Focus on key events and actions taken by this character."
+    query += "Do not reveal spoilers for later sections of the story."
+    response = client.chat(model=model, messages=[
+      {
+        'role': 'user',
+        'content': query,
+      },
+    ])
+    return response['message']['content']
+  summary = generate_summary()
+  has_spoiler = spoiler_check(book, character, summary, model)
+  while has_spoiler:
+    summary = generate_summary()
+    has_spoiler = spoiler_check(book, character, summary, model)
+  return summary
 
 
 def spoiler_check(book, character, summary, model):
@@ -59,11 +88,19 @@ def api_who_is_that():
     character = data.get("character")
     context = data.get("context")
 
+    # TODO: get the text of the book up to this point and the book name for Ed's function
+    # text = data.get("text")
+    # book = data.get("book")
+    # bookmark = data.get("context")
+
     if not character or not context:
         return jsonify({"error": "Character and context are required"}), 400
 
     try:
+        # Fede func
         result = who_is_that(context, prompt_template, character)
+        # Ed func
+        # result = who_is_that_really(text, book, bookmark, character, 'llama3')
         return jsonify({"result": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
