166505e6bec2c10b2bccfc8ffbd39f662bde2f15
James Robinson <james.em.robinson@gmail.com>
2024-06-11 16:49:15 +0100

    :recycle: Separate summarise into its own file

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,7 @@ import base64
 import json
 import os
 import requests
+from .summarise import get_context
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def index():
 @app.route("/", methods=["POST"])
 def load_file():
     uploaded_file = request.files["file"]
-    print(f"Loading file {uploaded_file.filename}")
+    print(f"Loading file {uploaded_file.filename}", flush=True)
 
     filestream = uploaded_file.stream
     filestream.seek(0)
@@ -33,9 +34,8 @@ def load_file():
     if isinstance(text_items[0], tuple):
         text_items = [content for _, content in text_items]
 
-    concatenated_text = "\n".join(
-        text_items
-    )  # Concatenate all text content into a single string
+    # Concatenate all text content into a single string
+    concatenated_text = "\n".join(text_items)
 
     cover_url = None
     if extractor.cover:
@@ -51,7 +51,8 @@ def load_file():
     return render_template(
         "process.html",
         text_items=[
-            ("Chapter " + str(i + 1), content) for i, content in enumerate(text_items)
+            ("Chapter " + str(idx), content)
+            for idx, content in enumerate(text_items, start=1)
         ],
         concatenated_text=concatenated_text,
         title=title,
@@ -60,26 +61,11 @@ def load_file():
     )
 
 
-def get_context(selected_text, request):
-    selected_text_start = int(request.form["selected_text_start"])
-    print(f"Finishing at position: {selected_text_start}")
-    concatenated_text = request.form["concatenated_text"]
-
-    print(
-        f"Summarising up to: {selected_text} starting at position: {selected_text_start}"
-    )
-
-    # Use the start position to slice the concatenated text
-    summary = concatenated_text[: selected_text_start + len(selected_text)]
-
-    return summary, concatenated_text
-
-
 @app.route("/summarise", methods=["POST"])
 def summarise():
     option = request.form["option"]
     selected_text = request.form["selected_text"]
-    print(f"Summarising up to: {selected_text}")
+    print(f"Summarising up to: {selected_text}", flush=True)
 
     author = request.form["author"]
     title = request.form["title"]
@@ -91,28 +77,20 @@ def summarise():
     url = f"{backend_host}:{backend_port}/who_is_that"
 
     # Create the payload
-    # Load context from file
-    context = "EXAMPLSE SOMETHING"
+    payload = {"character": selected_text, "context": summary}
 
-    # Define the character
-    character = "Mr. Wickham"
-
-    payload = {"character": character, "context": context}
     # Send the POST request
     try:
-        app.logger.info(f"Sending request to backend at '{url}'...")
+        print(f"Sending request to backend at '{url}'...", flush=True)
         response_who_is_that = requests.post(
             url, headers={"Content-Type": "application/json"}, data=json.dumps(payload)
         )
+        result = response_who_is_that.json()
     except Exception as exc:
-        print("Exception!!!!!!!!!!!!", flush=True)
-        print(payload["character"], flush=True)
-        print(url, flush=True)
-        print(f"Exception of type {type(exc)}", flush=True)
+        print(f"Failed to extract output {str(exc)}")
+        result = "Unknown"
 
     if option == "what_is_this" or option == "who_is_that":
-
-        result = response_who_is_that.json()
         return render_template(
             "process.html",
             text_items=[("What is this place?", result)],
@@ -124,7 +102,7 @@ def summarise():
     else:
         return render_template(
             "process.html",
-            text_items=[("Summary", summary)],
+            text_items=[("Summary", result)],
             concatenated_text=concatenated_text,
             title=title,
             author=author,
