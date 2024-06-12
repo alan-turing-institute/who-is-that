46925da556ae77f5c3f6befc38b82d8629f6f675
James Robinson <james.em.robinson@gmail.com>
2024-06-12 13:40:57 +0100

    :recycle: Simplify OllamaQuery interface

--- backend/helpers.py
+++ backend/helpers.py
@@ -18,14 +18,7 @@ def character_or_place(word: str, text: str) -> str:
     query += " I have used the word '" + word + "' in the story."
     query += " Determine whether this word refers to a character or a place."
     query += " Provide a simple answer of 'character' or 'place' or 'neither'."
-    response = OllamaQuery.query(
-        [
-            {
-                "role": "user",
-                "content": query,
-            },
-        ],
-    )
+    response = OllamaQuery.query(context=query)
     if "character" in response["message"]["content"]:
         return "character"
     if "place" in response["message"]["content"]:
@@ -46,14 +39,7 @@ def spoiler_check(book: str, character: str, summary: str) -> str:
     query += " Provide a simple answer of 'true' or 'false'."
     query += " If so, please provide another summary without spoilers."
 
-    response = OllamaQuery.query(
-        [
-            {
-                "role": "user",
-                "content": query,
-            },
-        ],
-    )
+    response = OllamaQuery.query(context=query)
     answer = response["message"]["content"]
     if "true" in answer or "True" in answer or "TRUE" in answer:
         print("Spoiler detected! Regenerating summary...")
@@ -68,14 +54,7 @@ def who_is_that(context: str, prompt_template: str, character: str) -> str:
     prompt = prompt_template.replace("{character}", character)
     concat = f"CONTEXT: {context} \n INSTRUCTIONS: {prompt}"
     try:
-        response = OllamaQuery.query(
-            [
-                {
-                    "role": "user",
-                    "content": concat,
-                },
-            ],
-        )
+        response = OllamaQuery.query(context=concat)
         content = response["message"]["content"]
 
         # content = spoiler_check(context, character, content)
@@ -91,14 +70,7 @@ def generate_summary(context: str) -> str:
     prompt = f"CONTEXT: {context}. \n INSTRUCTIONS: Summarize the story so far in 100 words or less. Do not reveal spoilers for later sections of the story."
     try:
         print("Waiting for an Ollama response.", flush=True)
-        response = OllamaQuery.query(
-            [
-                {
-                    "role": "user",
-                    "content": prompt,
-                },
-            ],
-        )
+        response = OllamaQuery.query(context=prompt)
         content = response["message"]["content"]
 
     except Exception as exc:
--- backend/ollama_query.py
+++ backend/ollama_query.py
@@ -1,37 +1,48 @@
 from __future__ import annotations
 
+import logging
 import os
 from collections.abc import Iterator
 from typing import Any, Mapping, Self
 
-from ollama import Client
-
+from ollama import Client, ResponseError
 
 class OllamaQuery:
+    logger = logging.getLogger("backend.app")
+
     @classmethod
     def query(
         cls: type[Self],
-        messages: list[dict[str, str]],
+        context: str,
     ) -> Mapping[str, Any] | Iterator[Mapping[str, Any]]:
 
+        # Load the model
         model = os.environ.get("OLLAMA_MODEL", "llama3:8b")
-        print(f"Querying Ollama using model {model}", flush=True)
+        cls.logger.info(f"Querying Ollama using model {model}")
 
+        # Set up connection to Ollama
         ollama_host = os.environ.get("OLLAMA_HOST", "http://localhost")
         ollama_port = os.environ.get("OLLAMA_PORT", "11434")
         ollama_server = f"{ollama_host}:{ollama_port}"
         client = Client(host=ollama_server)
-        print(f"Using Ollama server at {ollama_server}", flush=True)
+        cls.logger.info(f"Using Ollama server at {ollama_server}")
+
+        # Query Ollama
         try:
             response = client.chat(
                 model=model,
-                messages=messages,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": context,
+                    },
+                ],
             )
-            print(f"Loading the model: {response['load_duration']/1e9:.2f}s", flush=True)
-            print(f"Evaluating the prompt: {response['prompt_eval_duration']/1e9:.2f}s", flush=True)
-            print(f"Evaluating the response: {response['eval_duration']/1e9:.2f}s", flush=True)
-            print(f"Total time: {response['total_duration']/1e9:.2f}s", flush=True)
-        except Exception as exc:
-            print(f"Caught exception of type {type(exc)}")
-            response = {"message": {"content": "No response from Ollama slower."}}
+            cls.logger.info(f"Loading the model: {response['load_duration']/1e9:.2f}s")
+            cls.logger.info(f"Evaluating the prompt: {response['prompt_eval_duration']/1e9:.2f}s")
+            cls.logger.info(f"Evaluating the response: {response['eval_duration']/1e9:.2f}s")
+            cls.logger.info(f"Total time: {response['total_duration']/1e9:.2f}s")
+        except ResponseError as exc:
+            cls.logger.info(f"No response from Ollama server: '{exc}'")
+            response = {"message": {"content": "No response from Ollama server."}}
         return response
