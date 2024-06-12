510860d88d49836bb229275a694df271c0523d6a
Radka Jersakova <r.jersakova@gmail.com>
2024-06-12 14:41:23 +0100

    inherit style from index.html

--- frontend/app.py
+++ frontend/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html")
+    return render_template("load.html")
 
 
 @app.route("/", methods=["POST"])
