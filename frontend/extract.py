bb7b3f4a1abf34ee91106aebf3edba42e3974c03
James Robinson <james.em.robinson@gmail.com>
2024-06-12 14:24:52 +0100

    :loud_sound: Suppress warnings when reading book

--- frontend/extract.py
+++ frontend/extract.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+import warnings
 
 import pathlib
 import tempfile
 import typing
 
 import ebooklib
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning
 from ebooklib import epub
 
 
@@ -23,29 +24,28 @@ class Extractor:
         self.title = title
 
     @staticmethod
-    def get_metadata(epub_path: pathlib.Path, data_type: str) -> str:
-        book = epub.read_epub(epub_path, {"ignore_ncx": False})
+    def get_metadata(book: epub.EpubBook, data_type: str) -> str:
         metadata = book.get_metadata("DC", data_type)
         if metadata:
             return [md[0] for md in metadata]
         return f"{data_type} not found"
 
     @staticmethod
-    def get_cover(epub_path: pathlib.Path) -> None:
-        book = epub.read_epub(epub_path)
+    def get_cover(book: epub.EpubBook) -> None:
+        # book = epub.read_epub(epub_path)
         for item in book.get_items():
             if item.get_type() == ebooklib.ITEM_COVER:
                 return item.get_content()
         return None
 
     @staticmethod
-    def process(epub_path: pathlib.Path) -> list[tuple[str, str]]:
-        book = epub.read_epub(epub_path)
+    def process(book: epub.EpubBook) -> list[tuple[str, str]]:
         text_content = []
-
         for item in book.get_items():
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                soup = BeautifulSoup(item.get_content(), features="lxml")
+                with warnings.catch_warnings():
+                    warnings.filterwarnings(action="ignore", category=XMLParsedAsHTMLWarning)
+                    soup = BeautifulSoup(item.get_content(), features="lxml")
                 for container in soup.find_all(attrs={"epub:type": "chapter"}):
                     text_content.append((container["id"], container.get_text()))
 
@@ -55,18 +55,20 @@ class Extractor:
     def from_bytes(cls: type[typing.Self], epub_contents: bytes) -> Extractor:
         tf = tempfile.NamedTemporaryFile()
         tf.write(epub_contents)
-        return cls(
-            text_content=cls.process(tf.name),
-            cover=cls.get_cover(tf.name),
-            authors=cls.get_metadata(tf.name, "creator"),
-            title=cls.get_metadata(tf.name, "title")[0],
-        )
+        return cls.from_path(tf.name)
 
     @classmethod
     def from_path(cls: type[typing.Self], epub_path: pathlib.Path) -> Extractor:
+        # Read book ignoring warnings
+        with warnings.catch_warnings():
+            warnings.filterwarnings(action="ignore", category=UserWarning)
+            warnings.filterwarnings(action="ignore", category=FutureWarning)
+            book = epub.read_epub(epub_path, {"ignore_ncx": False})
+
+        # Construct a book
         return cls(
-            text_content=cls.process(epub_path),
-            cover=cls.get_cover(epub_path),
-            authors=cls.get_metadata(epub_path, "creator"),
-            title=cls.get_metadata(epub_path, "title")[0],
+            text_content=cls.process(book),
+            cover=cls.get_cover(book),
+            authors=cls.get_metadata(book, "creator"),
+            title=cls.get_metadata(book, "title")[0],
         )
